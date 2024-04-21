007c53d746eefc39adf337cbe8e7d909d843d395
harishpatel414 <58157049+harishpatel414@users.noreply.github.com>
2024-04-21 05:32:00 +0000

    conversion of number,strings,boolean,typeof

--- 1_basics/03_datatype..js
+++ 1_basics/03_datatype..js
@@ -1,1 +1,2 @@
 console.log(typeof null) //null is object
+console.log(typeof undefined) //undefined in data types undefined
---
+++ 1_basics/04_conversionOperation.js
@@ -0,0 +1,25 @@
+//number conversion
+
+let score = 33
+let score_string = "33abc"
+
+let conver = Number(score_string)
+//console.log(conver)
+
+let name_my = 33
+let number_conver = String(name_my)
+//console.log(typeof number_conver)
+//console.log(number_conver)
+let conversion = Boolean(name_my)
+console.log(conversion)
+let boolean_value = true
+
+//console.log(typeof boolean_value)
+
+//console.log(typeof score)
+//console.log(typeof score_string)
+
+
+//"33" => 33
+//"33abc" => NaN 
+//true => 1; false => 0
